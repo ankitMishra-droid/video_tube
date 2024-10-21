c99ace70462285ea246e47a0c588c9df9dba3d8e
mishraankit9653@gmail.com <mishraankit9653@gmail.com>
2024-10-21 17:20:01 +0530

    update

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -4,6 +4,7 @@ import { ApiError } from "../utils/ApiError.js";
 import User from "../models/users.model.js";
 import { Video } from "../models/video.model.js";
 import crypto from "crypto";
+import mongoose, { isValidObjectId } from "mongoose";
 
 const publishVideo = asyncHandler(async (req, res) => {
     try {
@@ -56,4 +57,230 @@ const publishVideo = asyncHandler(async (req, res) => {
     }
 });
 
-export { publishVideo };
+const getAllVideos = asyncHandler( async(req, res) => {
+    try {
+        const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
+
+        const sortByField = ["createdAt", "duration", "views"];
+        const sortTypeArr = ["asc", "dsc"];
+
+        const getVideo = await Video.aggregate([
+            {
+                $match: {
+                    $or: [
+                        {
+                            onwer: userId ? new mongoose.Types.ObjectId(userId) : null
+                        },
+                        {
+                            $and: [
+                                { isPublished: true },
+                                {
+                                    $or: [
+                                        {
+                                            title: query ? { $regex: query, $options: "i" } : { $exists: true }
+                                        },
+                                        {
+                                            description: query ? { $regex: query, $options: "i" } : null
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "owner",
+                    foreignField: "_id",
+                    as: "owner",
+                    pipeline: [
+                        {
+                            $project:{
+                                userName: 1,
+                                firstName: 1,
+                                avatar: 1
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                $sort: {
+                    [sortBy]: sortType === "dsc" ? -1 : 1
+                }
+            },
+            {
+                $addFields: {
+                    owner: {
+                        $first: "$owner"
+                    }
+                }
+            }
+        ])
+
+        const result = await Video.aggregatePaginate(getVideo, {
+            page,
+            limit,
+            customLabels: {
+                totalDocs: "totalVideos",
+                docs: "Videos",
+            },
+            allowDiskUse: true
+        })
+
+        if(result.totalPages === 0){
+            throw new ApiError(404, "videos not found")
+        }
+
+        return res.status(200).json(
+            new ApiResponse(201, "vidoes fetched.", result)
+        )
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(
+            new ApiError(500, "somthing went wrong")
+        )
+    }
+})
+
+const getVideoById = asyncHandler( async(req, res) => {
+    try {
+        const { videoId } = req.params;
+
+        if(!isValidObjectId(videoId)){
+            throw new ApiError(404, "invalid videoID")
+        }
+
+        const userId = req.user?._id;
+        if(!userId){
+            throw new ApiError(404, "invalid user")
+        }
+
+        const fetchVideo = await Video.aggregate([
+            {
+                $match: {
+                    _id: new mongoose.Types.ObjectId(videoId)
+                }
+            },
+            {
+                $lookup: {
+                    from: "like",
+                    localField: "_id",
+                    foreignField: "video",
+                    as: "likes"
+                }
+            },
+            {
+                $lookup: {
+                    from:"User",
+                    localField: "owner",
+                    foreignField: "_id",
+                    as: "owner",
+                    pipeline: [
+                        {
+                            $lookup: {
+                                from: "Subscription",
+                                localField: "_id",
+                                foreignField: "channel",
+                                as: "subscribers" 
+                            },
+                        },
+                        {
+                            $addFields: {
+                                subscriberCount: {
+                                    $size: "$subscribers"
+                                },
+                                isSubscribed: {
+                                    $cond: {
+                                        $if: {
+                                            $in:[
+                                                req.user._id,
+                                                "$subscribers.subscriber"
+                                            ]
+                                        },
+                                        then: true,
+                                        else: false
+                                    }
+                                }
+                            }
+                        },
+                        {
+                            $project: {
+                                userName: 1,
+                                "avatar.url": 1,
+                                subscriberCount: 1,
+                                isSubscribed: 1
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                $addFields: {
+                    likeCount: {
+                        $size: "$likes"
+                    },
+                    owner: {
+                        $first: owner
+                    },
+                    isLiked:{
+                        $cond: {
+                            $if: {
+                                $in: [
+                                    req.user?._id, "$likes.likedBy"
+                                ]
+                            },
+                            then: true,
+                            else: false
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    "videoFile.url":1,
+                    title : 1,
+                    description: 1,
+                    createdAt :1,
+                    views: 1,
+                    subscriberCount: 1,
+                    likeCount: 1,
+                    isLiked: 1,
+                    duration: 1,
+                    commetns: 1  
+                }
+            }
+        ])
+
+        if(!fetchVideo){
+            throw new ApiError(404, "video dosen't availabe")
+        }
+
+        // increase views
+        await Video.findByIdAndUpdate(
+            videoId, {
+                $inc: {views: 1}
+            }
+        );
+
+        // store watchHistory
+        await Video.findByIdAndUpdate(req.user?._id, {
+            $addToSet: {
+                watchHistory: videoId
+            }
+        })
+
+        return res.status(200).json(
+            new ApiResponse(201, "video fetched", fetchVideo)
+        )
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(
+            new ApiError(500, "something went wrong")
+        )
+    }
+})
+
+export { publishVideo, getAllVideos, getVideoById };
--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.js";
-import { publishVideo } from "../controllers/video.controller.js";
+import { getAllVideos, getVideoById, publishVideo } from "../controllers/video.controller.js";
 
 const videoRoutes = Router();
 
-videoRoutes.route("/upload-video").post(verifyJWT, upload.fields([
+videoRoutes.route("/").post(verifyJWT, upload.fields([
     {
         name: "videoFile",
         maxCount: 1
@@ -14,6 +14,8 @@ videoRoutes.route("/upload-video").post(verifyJWT, upload.fields([
         name: "thumbnail",
         maxCount: 1
     }
-]), publishVideo)
+]), publishVideo).get(getAllVideos)
+
+videoRoutes.route("/:videoId").get(verifyJWT, getVideoById)
 
 export { videoRoutes }
