ec32d834c09930a01930af2613fab23c4eaa4360
Ankit Mishra <mishraankit9653@gmail.com>
2025-01-02 13:59:38 +0530

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,8 +11,8 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({ limit: "300mb" }));
-app.use(express.urlencoded({ extended: true, limit: "300mb" }));
+app.use(express.json({ limit: "500mb" }));
+app.use(express.urlencoded({ extended: true, limit: "500mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
