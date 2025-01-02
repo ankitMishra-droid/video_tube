3d22f5c9c0356a9e3ea076411af1853d94ce94f4
Ankit Mishra <mishraankit9653@gmail.com>
2025-01-02 16:08:11 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,31 +3,17 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => {
+
   return {
     plugins: [react()],
     server: {
-      // Only set proxy for development mode
-      proxy: mode === "development" 
-        ? {
-            "/api": {
-              target: process.env.VITE_BACKEND_URL_DEV, // Proxy to backend in dev
-              changeOrigin: true, // Change origin to match the target
-              rewrite: (path) => path.replace(/^\/api/, ""), // Optionally remove /api prefix
-            },
-          }
-        : {},
+      proxy: {
+        "/api": "https://video-tube-indol.vercel.app/api",
+      }
     },
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
-      },
-    },
-    build: {
-      // In production, make sure API calls directly hit the production API URL
-      define: {
-        "process.env": {
-          VITE_BACKEND_URL: process.env.VITE_BACKEND_URL_PROD, // Direct API URL for production
-        },
       },
     },
   };
